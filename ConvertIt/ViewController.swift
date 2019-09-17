4d1d393ea32f57030612ec1a033e68e35b7f9243
Zach Ogden <ogdenza@bc.edu>
2019-09-17 15:26:54 -0400

    6.3 Completed

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var fromUnitsLabel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var formulaPicker: UIPickerView!
+    
+    
+    var formulaArray = ["miles to kilometers",
+                        "kilometers to miles",
+                        "feet to meters",
+                        "meters to feet",
+                        "meters to yards"]
+    
+    var fromUnits = ""
+    var toUnits = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        formulaPicker.delegate = self
+        formulaPicker.dataSource = self
+        
     }
 
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+    }
 
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return formulaArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return formulaArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let unitsArray = formulaArray[row].components(separatedBy: " to ")
+        fromUnits = unitsArray[0]
+        toUnits = unitsArray[1]
+        fromUnitsLabel.text = fromUnits
+        resultsLabel.text = toUnits 
+        
+    }
+}
